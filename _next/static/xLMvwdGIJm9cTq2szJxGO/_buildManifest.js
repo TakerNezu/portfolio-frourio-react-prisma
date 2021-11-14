8880efd4e0e6be83adc2b8296d09d1ceac1691ec
TakerNezu <TakerNezu@users.noreply.github.com>
2021-11-14 15:40:01 +0000

    deploy: ea202aeb7c6476872e558816f84f0d70a9e1c269

---
+++ _next/static/xLMvwdGIJm9cTq2szJxGO/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/414-e48db32e21654be848b2.js","static/css/4012d39508e766ad6961.css","static/chunks/pages/index-1cc13f2ab89de195144f.js"],"/_error":["static/chunks/pages/_error-a9f53acb468cbab8a6cb.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
